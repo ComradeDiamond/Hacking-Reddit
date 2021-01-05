2486c686a3e59609ef38d659971847484bfea164
Justin <JustinChen3946@gmail.com>
2021-01-05 11:49:38 -0500

    testing

---
+++ Main.java
@@ -0,0 +1,9 @@
+import objects.items.*;
+import gameNav.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Game theGame = new Game();
+        theGame.playGame();
+    }
+}
